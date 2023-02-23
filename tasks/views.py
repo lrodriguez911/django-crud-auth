de420989eeeb24a5df2cbe16342ce00b85b317d7
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-02-22 23:02:10 -0300

    add management errors

--- tasks/views.py
+++ tasks/views.py
@@ -1,28 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
 
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, "home.html")
+
 
 def signup(request):
-
-    if request.method == 'GET':
-        return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm})
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            #register user
+        if request.POST["password1"] == request.POST["password2"]:
+            # register user
             try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
                 user.save()
-                return HttpResponse('User created successfully')
-            except:
-                return HttpResponse('Username already exists')
-        return HttpResponse
-        
-    
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "Username already exists"},
+                )
+        return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "Password do not match"},
+                )
+
+def tasks(request):
+    return render(request, "tasks.html")
